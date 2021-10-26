cb40e40797ca51e9bf8fe3e58e0f7c788d87a0d5
Gregory McGarry <g.mcgarry@ieee.org>
2021-10-27 00:05:26 +1100

    Flesh out the RISC-V backend.

---
+++ riscv/machdef.h
@@ -0,0 +1,34 @@
+#define LISTING
+#define THREE_PASS
+#if 0
+#define ASLD
+#endif
+
+/* ========== Machine independent type declarations ========== */
+
+#define PASS_1          0
+#define PASS_2          1
+#define PASS_3          2
+
+#define PASS_SYMB       (pass != PASS_1)
+#define PASS_RELO       (pass != PASS_1)
+
+#define IGNORECASE
+
+/*
+#define WORDS_REVERSED
+#define BYTES_REVERSED
+*/
+
+#define ADDR_T		long
+#define word_t		unsigned int
+
+#define ALIGNWORD	4
+#define ALIGNSECT	4
+#define VALWIDTH	8
+
+/* Some character constants for scanner*/
+#define ASC_COMMENT     ';'
+#define CTRL(x)         ((x) & 037)
+#define ISALPHA(c)      (isalpha(c) || (c) == '_')
+#define ISALNUM(c)      (isalnum(c) || (c) == '_')
