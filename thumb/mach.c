7d40771ac352d60b79c91d0bac8fc8430cf9a934
Gregory McGarry <g.mcgarry@ieee.org>
2021-11-09 22:50:52 +1100

    First pass at thumb backend. Probably more classic syntax than UAL syntax.

---
+++ thumb/mach.c
@@ -0,0 +1,84 @@
+/*-
+ * Copyright (c) 2021 Gregory McGarry <g.mcgarry@ieee.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "as.h"
+#include "error.h"
+
+int isa = ISA_ARMv7;
+
+extern sect_t sect[];
+
+extern sect_t sect[];
+extern int hash(const char *);
+
+static item_t cseg = { 0, 0, S_UND, ".text" };
+
+void
+mflag(const char* flag)
+{
+}
+
+void
+machstart(int pass)
+{
+	if (pass == PASS_1) {
+		item_insert(&cseg, hash(cseg.i_name));
+		unresolved++;
+	}
+	newsect(&cseg, 0, NULL);
+}
+
+void
+machfinish(int pass)
+{
+}
+
+void
+setcpu(const char* cpu)
+{
+	warning("ignoring .cpu directive (%s)", cpu);
+}
+
+void
+setarch(const char* arch)
+{
+	/*
+	 * armv4t
+	 * armv5t armv5te
+	 * armv6-m armv6s-m armv6t2
+	 * armv7-m armv7e-m
+	 */
+	if (strcmp(arch, "armv4t") == 0)
+		isa = ISA_ARMv4T;
+	else if (strncmp(arch, "arm5t", 5) == 0)
+		isa = ISA_ARMv5T;
+	else if (strncmp(arch, "armv6", 5) == 0)
+		isa = ISA_ARMv6;
+	else if (strncmp(arch, "armv7", 5) == 0)
+		isa = ISA_ARMv7;
+	else
+		fatal("unsupported '%s' ISA", arch);
+}
+
+void
+setfpu(const char* fpu)
+{
+	warning("ignoring .fpu directive (%s)", fpu);
+}
