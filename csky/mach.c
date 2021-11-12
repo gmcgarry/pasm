f4af6a997588bf8159c3743fb47b56a146f74215
Gregory McGarry <g.mcgarry@ieee.org>
2021-11-13 08:00:48 +1100

    First pass at CSKY instructions set as used in the W806 mcu.

---
+++ csky/mach.c
@@ -0,0 +1,49 @@
+/*-
+ * Copyright (c) 2021 Gregory McGarry <g.mcgarry@ieee.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "as.h"
+#include "error.h"
+
+extern sect_t sect[];
+
+extern sect_t sect[];
+extern int hash(const char *);
+
+static item_t cseg = { 0, S_UND, 0, ".text" };
+
+void
+mflag(const char* flag)
+{
+}
+
+void
+machstart(int pass)
+{
+	if (pass == PASS_1) {
+		item_insert(&cseg, H_GLOBAL+hash(cseg.i_name));
+		unresolved++;
+	}
+	newsect(&cseg, 0, NULL);
+}
+
+void
+machfinish(int pass)
+{
+}
---
+++ csky/machdef.h
@@ -0,0 +1,51 @@
+/*-
+ * Copyright (c) 2021 Gregory McGarry <g.mcgarry@ieee.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#define LISTING
+#define THREE_PASS
+
+/* ========== Machine independent type declarations ========== */
+
+#define PASS_1          0
+#define PASS_2          1
+#define PASS_3          2
+
+#define PASS_SYMB       (pass != PASS_1)
+#define PASS_RELO       (pass != PASS_1)
+
+#define IGNORECASE
+
+/*
+#define WORDS_REVERSED
+#define BYTES_REVERSED
+*/
+
+#define ADDR_T		long
+#define word_t		unsigned int
+
+#define WORDSIZE	4
+#define DWORDSIZE	8
+#define ALIGNWORD	4
+#define ALIGNSECT	4
+#define VALWIDTH	8
+
+/* Some character constants for scanner*/
+#define ASC_COMMENT     '#'
+#define CTRL(x)         ((x) & 037)
+#define ISALPHA(c)      (isalpha(c) || (c) == '_' || (c) == '.' || (c) == '%')
+#define ISALNUM(c)      (isalnum(c) || (c) == '_' || (c) == '.')
+
+#define ELFM		"%"
