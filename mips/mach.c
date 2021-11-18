92f67ebc9c7217de8192eb83a7d272ada900ae5b
Gregory McGarry <g.mcgarry@ieee.org>
2021-11-18 23:34:08 +1100

    Derived from a table-driven implementation by David Given.

---
+++ mips/mach.c
@@ -0,0 +1,18 @@
+#include "as.h"
+
+extern sect_t sect[];
+
+void
+mflag(const char* flag)
+{
+}
+
+void
+machstart(int pass)
+{
+}
+
+void
+machfinish(int pass)
+{
+}
---
+++ mips/mach.h
@@ -0,0 +1,4 @@
+#define FIXUPFLAGS	0
+#define RELOMIPS	1
+#define RELO2HI		2
+#define RELO2HISAD	2
