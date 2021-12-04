ddf265e5d9c73d6a59b8192ba5cdcec0495d8a07
Gregory McGarry <g.mcgarry@ieee.org>
2021-12-04 23:59:25 +1100

    Add enough csky support to succesfully compile blink.asm.

--- csky/mach.c
+++ csky/mach.c
@@ -47,3 +47,39 @@ void
 machfinish(int pass)
 {
 }
+
+
+static ADDR_T literals_address;
+static ADDR_T literals[1024];
+static int nliterals;
+
+ADDR_T
+add_literal(ADDR_T v)
+{
+	int i;
+
+	DPRINTF(("pass %d, adding literal[%d]=0x%x @ 0x%x\n", pass, nliterals, v, literals_address+nliterals*4));
+	for (i = 0; i < nliterals; i++) {
+		if (literals[i] == v)
+			return literals_address + i * 4;
+	}
+	literals[nliterals] = v;
+	return literals_address + nliterals++ * 4;
+}
+
+void
+emit_literals()
+{
+	int i = 0;
+
+	if (nliterals == 0)
+		return;
+
+	align(4,0,0);
+	literals_address = DOTVAL;
+	for (i = 0; i < nliterals; i++) {
+		DPRINTF(("pass %d, emitting literal[%d]=0x%x @ 0x%x\n", pass, i, literals[i], literals_address+(i*4)));
+		emit4(literals[i]);
+	}
+	nliterals = 0;
+}
